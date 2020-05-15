620cab80a2d4db9ddcaf0e04bc10a0d058c5a132
Dan Gerchcovich <dan.developer789@gmail.com>
2020-05-15 17:13:18 +1000

    master changes

--- Calculator/Maven_2/src/main/java/Maven_4/App.java
+++ Calculator/Maven_2/src/main/java/Maven_4/App.java
@@ -1,9 +1,47 @@
 package Maven_4;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+
+                Platform.exit();
+            }
+        });
+
+        FlowPane root = new FlowPane();
+        root.setOrientation(Orientation.VERTICAL);
+        root.getChildren().add(btn);
+        root.getChildren().add(new Hyperlink("www.gmail.com"));
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("MAHAAHHAAH!");
     }
 }
---
+++ Calculator/Maven_2/src/main/java/Maven_4/Dashboard.java
@@ -0,0 +1,7 @@
+package Maven_4;
+
+public class Dashboard {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
